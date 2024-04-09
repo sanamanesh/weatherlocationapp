73199cfcd8df433836fc131788365a5c8e266b7b
sanamanesh <sanmanesh@gmail.com>
2024-04-08 23:30:27 -0400

    Initial Commit

---
+++ hw4/hw4App.swift
@@ -0,0 +1,17 @@
+//
+//  hw4App.swift
+//  hw4
+//
+//  Created by Sana Manesh on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct hw4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
